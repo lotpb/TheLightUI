1db135f5e6e1532a75bba8998e8d35c0915d9651
Peter Balsamo <eunited@optonline.net>
2021-06-14 18:41:41 -0400

    Initial Commit

---
+++ Shared/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  Shared
+//
+//  Created by Peter Balsamo on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
