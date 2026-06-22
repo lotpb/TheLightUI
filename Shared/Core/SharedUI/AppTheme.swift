595ab18b8563219ee5ab7c238e7599de020d8bc2
Peter Balsamo <eunitedws@icloud.com>
2026-06-22 10:35:20 -0400

    fix warnings

--- Shared/Core/SharedUI/AppTheme.swift
+++ Shared/Core/SharedUI/AppTheme.swift
@@ -7,6 +7,17 @@ import SwiftUI
 
 enum AppTheme {
     static func accentColor(for storedColor: Int?) -> Color {
-        storedColor == 0 ? .purple : .orange
+        switch storedColor {
+        case 0:
+            return .purple
+        case 1:
+            return .orange
+        case 2:
+            return .blue
+        case 3:
+            return .indigo
+        default:
+            return .purple
+        }
     }
 }
