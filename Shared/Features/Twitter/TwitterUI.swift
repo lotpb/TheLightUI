bafbcca628fe283f6ec80b3604428679efdc8d0d
Peter Balsamo <eunitedws@icloud.com>
2026-06-30 16:12:48 -0400

    fix errors

--- Shared/Features/Twitter/TwitterUI.swift
+++ Shared/Features/Twitter/TwitterUI.swift
@@ -268,6 +268,7 @@ private struct TabButton: View {
     }
 }
 
+@available(iOS 18.0, *)
 private struct TweetView: View {
     static let sampleText = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book."
 
